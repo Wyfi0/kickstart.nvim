4088387d5d26725e1dbe2f845f331ec00f1d0437
Wyfi0 <wyfi08g@proton.me>
2025-05-29 19:02:04 -0400

    switched to indent-o-matic instead of guess-indent

--- init.lua
+++ init.lua
@@ -191,7 +191,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  'Darazaki/indent-o-matic', -- Detect tabstop and shiftwidth automatically
   'tpope/vim-fugitive', -- A git wrapper for nvim
 
   -- NOTE: Plugins can also be added by using a table,
